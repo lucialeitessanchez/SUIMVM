6ae6c0af4a173afeca11c261779910984f9bfc7b
vvazquez <vvazquez@santafe.gov.ar>
2025-07-15 12:36:31 -0300

    entidades sddnayf

---
+++ src/Entity/SddnayfHijosVictima.php
@@ -0,0 +1,345 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use App\Entity\Nomenclador;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'sddnayf_hijos_victima')]
+class SddnayfHijosVictima
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer', name: 'id_vinculado')]
+    private ?int $idVinculado = null;
+
+    #[ORM\Column(type: 'string', length: 55, nullable: true)]
+    private ?string $apenom = null;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private ?\DateTimeInterface $fnac = null;
+
+    #[ORM\Column(type: 'string', length: 10, nullable: true)]
+    private ?string $nrodocu = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $vinculoAgresor = null;
+
+    #[ORM\Column(type: 'boolean', nullable: true, name: 'sddnayf_3a')]
+    private ?bool $sddnayf3a = null;
+
+    #[ORM\Column(type: 'integer', nullable: true, name: 'sddnayf_3d')]
+    private ?int $sddnayf3d = null;
+
+    #[ORM\Column(type: 'date', nullable: true, name: 'sddnayf_3fa')]
+    private ?\DateTimeInterface $sddnayf3fa = null;
+
+    #[ORM\Column(type: 'date', nullable: true, name: 'sddnayf_3fb')]
+    private ?\DateTimeInterface $sddnayf3fb = null;
+
+    #[ORM\Column(type: 'integer', nullable: true, name: 'sddnayf_3g')]
+    private ?int $sddnayf3g = null;
+
+    #[ORM\Column(type: 'integer', nullable: true, name: 'sddnayf_3h')]
+    private ?int $sddnayf3h = null;
+
+    #[ORM\Column(type: 'boolean', nullable: true, name: 'sddnayf_3i')]
+    private ?bool $sddnayf3i = null;
+
+    #[ORM\Column(type: 'boolean', nullable: true, name: 'sddnayf_3j')]
+    private ?bool $sddnayf3j = null;
+
+    #[ORM\Column(type: 'boolean', nullable: true, name: 'sddnayf_3k')]
+    private ?bool $sddnayf3k = null;
+
+    // Relación ManyToOne hacia SddnayfNew
+
+    #[ORM\ManyToOne(targetEntity: SddnayfNew::class, inversedBy: 'hijosVictima')]
+    #[ORM\JoinColumn(name: 'sddnayf_new_id', referencedColumnName: 'id', nullable: false)]
+    private ?SddnayfNew $sddnayfNew = null;
+
+
+    //ManyToMany
+    #[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+    #[ORM\JoinTable(
+        name: 'sddnayf_3b',
+        joinColumns: [new ORM\JoinColumn(name: 'hijo_id', referencedColumnName: 'id_vinculado')],
+        inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+    )]
+    private Collection $sddnayf3b;
+
+    #[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+    #[ORM\JoinTable(
+        name: 'sddnayf_3c',
+        joinColumns: [new ORM\JoinColumn(name: 'hijo_id', referencedColumnName: 'id_vinculado')],
+        inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+    )]
+    private Collection $sddnayf3c;
+
+    #[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+    #[ORM\JoinTable(
+        name: 'sddnayf_3e',
+        joinColumns: [new ORM\JoinColumn(name: 'hijo_id', referencedColumnName: 'id_vinculado')],
+        inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+    )]
+    private Collection $sddnayf3e;
+
+    #[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+    #[ORM\JoinTable(
+        name: 'sddnayf_3l',
+        joinColumns: [new ORM\JoinColumn(name: 'hijo_id', referencedColumnName: 'id_vinculado')],
+        inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+    )]
+    private Collection $sddnayf3l;
+
+    public function __construct()
+{
+    $this->sddnayf3b = new ArrayCollection();
+    $this->sddnayf3c = new ArrayCollection();
+    $this->sddnayf3e = new ArrayCollection();
+    $this->sddnayf3l = new ArrayCollection();
+}
+
+    // Getters y Setters
+
+    public function getIdVinculado(): ?int
+    {
+        return $this->idVinculado;
+    }
+
+    public function getApenom(): ?string
+    {
+        return $this->apenom;
+    }
+
+    public function setApenom(?string $apenom): self
+    {
+        $this->apenom = $apenom;
+        return $this;
+    }
+
+    public function getFnac(): ?\DateTimeInterface
+    {
+        return $this->fnac;
+    }
+
+    public function setFnac(?\DateTimeInterface $fnac): self
+    {
+        $this->fnac = $fnac;
+        return $this;
+    }
+
+    public function getNrodocu(): ?string
+    {
+        return $this->nrodocu;
+    }
+
+    public function setNrodocu(?string $nrodocu): self
+    {
+        $this->nrodocu = $nrodocu;
+        return $this;
+    }
+
+    public function getVinculoAgresor(): ?int
+    {
+        return $this->vinculoAgresor;
+    }
+
+    public function setVinculoAgresor(?int $vinculoAgresor): self
+    {
+        $this->vinculoAgresor = $vinculoAgresor;
+        return $this;
+    }
+
+    public function getSddnayf3a(): ?bool
+    {
+        return $this->sddnayf3a;
+    }
+
+    public function setSddnayf3a(?bool $sddnayf3a): self
+    {
+        $this->sddnayf3a = $sddnayf3a;
+        return $this;
+    }
+
+    public function getSddnayf3d(): ?int
+    {
+        return $this->sddnayf3d;
+    }
+
+    public function setSddnayf3d(?int $sddnayf3d): self
+    {
+        $this->sddnayf3d = $sddnayf3d;
+        return $this;
+    }
+
+    public function getSddnayf3fa(): ?\DateTimeInterface
+    {
+        return $this->sddnayf3fa;
+    }
+
+    public function setSddnayf3fa(?\DateTimeInterface $sddnayf3fa): self
+    {
+        $this->sddnayf3fa = $sddnayf3fa;
+        return $this;
+    }
+
+    public function getSddnayf3fb(): ?\DateTimeInterface
+    {
+        return $this->sddnayf3fb;
+    }
+
+    public function setSddnayf3fb(?\DateTimeInterface $sddnayf3fb): self
+    {
+        $this->sddnayf3fb = $sddnayf3fb;
+        return $this;
+    }
+
+    public function getSddnayf3g(): ?int
+    {
+        return $this->sddnayf3g;
+    }
+
+    public function setSddnayf3g(?int $sddnayf3g): self
+    {
+        $this->sddnayf3g = $sddnayf3g;
+        return $this;
+    }
+
+    public function getSddnayf3h(): ?int
+    {
+        return $this->sddnayf3h;
+    }
+
+    public function setSddnayf3h(?int $sddnayf3h): self
+    {
+        $this->sddnayf3h = $sddnayf3h;
+        return $this;
+    }
+
+    public function getSddnayf3i(): ?bool
+    {
+        return $this->sddnayf3i;
+    }
+
+    public function setSddnayf3i(?bool $sddnayf3i): self
+    {
+        $this->sddnayf3i = $sddnayf3i;
+        return $this;
+    }
+
+    public function getSddnayf3j(): ?bool
+    {
+        return $this->sddnayf3j;
+    }
+
+    public function setSddnayf3j(?bool $sddnayf3j): self
+    {
+        $this->sddnayf3j = $sddnayf3j;
+        return $this;
+    }
+
+    public function getSddnayf3k(): ?bool
+    {
+        return $this->sddnayf3k;
+    }
+
+    public function setSddnayf3k(?bool $sddnayf3k): self
+    {
+        $this->sddnayf3k = $sddnayf3k;
+        return $this;
+    }
+
+    public function getSddnayfNew(): ?SddnayfNew
+    {
+        return $this->sddnayfNew;
+    }
+
+    public function setSddnayfNew(?SddnayfNew $sddnayfNew): self
+    {
+        $this->sddnayfNew = $sddnayfNew;
+        return $this;
+    }
+
+    //Add y remove
+        public function getSddnayf3b(): Collection
+    {
+        return $this->sddnayf3b;
+    }
+
+    public function addSddnayf3b(Nomenclador $nomenclador): self
+    {
+        if (!$this->sddnayf3b->contains($nomenclador)) {
+            $this->sddnayf3b->add($nomenclador);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf3b(Nomenclador $nomenclador): self
+    {
+        $this->sddnayf3b->removeElement($nomenclador);
+        return $this;
+    }
+    //----
+        public function getSddnayf3c(): Collection
+    {
+        return $this->sddnayf3c;
+    }
+
+    public function addSddnayf3c(Nomenclador $nomenclador): self
+    {
+        if (!$this->sddnayf3c->contains($nomenclador)) {
+            $this->sddnayf3c->add($nomenclador);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf3c(Nomenclador $nomenclador): self
+    {
+        $this->sddnayf3c->removeElement($nomenclador);
+        return $this;
+    }
+
+    //---
+    public function getSddnayf3e(): Collection
+    {
+        return $this->sddnayf3e;
+    }
+
+    public function addSddnayf3e(Nomenclador $nomenclador): self
+    {
+        if (!$this->sddnayf3e->contains($nomenclador)) {
+            $this->sddnayf3e->add($nomenclador);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf3e(Nomenclador $nomenclador): self
+    {
+        $this->sddnayf3e->removeElement($nomenclador);
+        return $this;
+    }
+    //-----
+    public function getSddnayf3l(): Collection
+    {
+        return $this->sddnayf3l;
+    }
+
+    public function addSddnayf3l(Nomenclador $nomenclador): self
+    {
+        if (!$this->sddnayf3l->contains($nomenclador)) {
+            $this->sddnayf3l->add($nomenclador);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf3l(Nomenclador $nomenclador): self
+    {
+        $this->sddnayf3l->removeElement($nomenclador);
+        return $this;
+    }
+
+}
---
+++ src/Entity/SddnayfNew.php
@@ -0,0 +1,442 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use App\Entity\Nomenclador;
+use App\Entity\SddnayfHijosVictima;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'sddnayf_new')]
+class SddnayfNew
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_1a = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_1d = null;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private ?\DateTimeInterface $sddnayf_1fa = null;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private ?\DateTimeInterface $sddnayf_1fb = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_1g = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_1h = null;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    private ?bool $sddnayf_1i = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_2a = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_2d = null;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private ?\DateTimeInterface $sddnayf_2fa = null;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private ?\DateTimeInterface $sddnayf_2fb = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_2g = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sddnayf_2h = null;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    private ?bool $sddnayf_2i = null;
+
+    //oneTomany
+    #[ORM\OneToMany(mappedBy: 'sddnayfNew', targetEntity: SddnayfHijosVictima::class, cascade: ['persist', 'remove'], orphanRemoval: true)]
+    private Collection $hijosVictima;
+
+
+//manyTomany
+
+#[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+#[ORM\JoinTable(
+    name: 'sddnayf_1b',
+    joinColumns: [new ORM\JoinColumn(name: 'sddnayf_id', referencedColumnName: 'id')],
+    inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+)]
+private Collection $sddnayf_1b;
+
+#[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+#[ORM\JoinTable(
+    name: 'sddnayf_1c',
+    joinColumns: [new ORM\JoinColumn(name: 'sddnayf_id', referencedColumnName: 'id')],
+    inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+)]
+private Collection $sddnayf_1c;
+
+//item 2
+#[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+#[ORM\JoinTable(
+    name: 'sddnayf_2b',
+    joinColumns: [new ORM\JoinColumn(name: 'sddnayf_id', referencedColumnName: 'id')],
+    inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+)]
+private Collection $sddnayf_2b;
+
+#[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+#[ORM\JoinTable(
+    name: 'sddnayf_2c',
+    joinColumns: [new ORM\JoinColumn(name: 'sddnayf_id', referencedColumnName: 'id')],
+    inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+)]
+private Collection $sddnayf_2c;
+
+#[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+#[ORM\JoinTable(
+    name: 'sddnayf_2e',
+    joinColumns: [new ORM\JoinColumn(name: 'sddnayf_id', referencedColumnName: 'id')],
+    inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+)]
+private Collection $sddnayf_2e;
+
+#[ORM\ManyToMany(targetEntity: Nomenclador::class)]
+#[ORM\JoinTable(
+    name: 'sddnayf_1e',
+    joinColumns: [new ORM\JoinColumn(name: 'sddnayf_id', referencedColumnName: 'id')],
+    inverseJoinColumns: [new ORM\JoinColumn(name: 'nomenclador_id', referencedColumnName: 'id')]
+)]
+private Collection $sddnayf_1e;
+
+public function __construct()
+    {
+        $this->sddnayf_1b = new ArrayCollection();
+        $this->sddnayf_1c = new ArrayCollection();
+        $this->sddnayf_1e = new ArrayCollection();
+        $this->sddnayf_2b = new ArrayCollection();
+        $this->sddnayf_2c = new ArrayCollection();
+        $this->sddnayf_2e = new ArrayCollection();
+        $this->hijosVictima = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection|SddnayfHijosVictima[]
+     */
+
+    // Getters y Setters
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSddnayf1a(): ?int
+    {
+        return $this->sddnayf_1a;
+    }
+
+    public function setSddnayf1a(?int $sddnayf_1a): self
+    {
+        $this->sddnayf_1a = $sddnayf_1a;
+        return $this;
+    }
+
+    public function getSddnayf1d(): ?int
+    {
+        return $this->sddnayf_1d;
+    }
+
+    public function setSddnayf1d(?int $sddnayf_1d): self
+    {
+        $this->sddnayf_1d = $sddnayf_1d;
+        return $this;
+    }
+
+    public function getSddnayf1fa(): ?\DateTimeInterface
+    {
+        return $this->sddnayf_1fa;
+    }
+
+    public function setSddnayf1fa(?\DateTimeInterface $sddnayf_1fa): self
+    {
+        $this->sddnayf_1fa = $sddnayf_1fa;
+        return $this;
+    }
+
+    public function getSddnayf1fb(): ?\DateTimeInterface
+    {
+        return $this->sddnayf_1fb;
+    }
+
+    public function setSddnayf1fb(?\DateTimeInterface $sddnayf_1fb): self
+    {
+        $this->sddnayf_1fb = $sddnayf_1fb;
+        return $this;
+    }
+
+    public function getSddnayf1g(): ?int
+    {
+        return $this->sddnayf_1g;
+    }
+
+    public function setSddnayf1g(?int $sddnayf_1g): self
+    {
+        $this->sddnayf_1g = $sddnayf_1g;
+        return $this;
+    }
+
+    public function getSddnayf1h(): ?int
+    {
+        return $this->sddnayf_1h;
+    }
+
+    public function setSddnayf1h(?int $sddnayf_1h): self
+    {
+        $this->sddnayf_1h = $sddnayf_1h;
+        return $this;
+    }
+
+    public function getSddnayf1i(): ?bool
+    {
+        return $this->sddnayf_1i;
+    }
+
+    public function setSddnayf1i(?bool $sddnayf_1i): self
+    {
+        $this->sddnayf_1i = $sddnayf_1i;
+        return $this;
+    }
+
+    public function getSddnayf2a(): ?int
+    {
+        return $this->sddnayf_2a;
+    }
+
+    public function setSddnayf2a(?int $sddnayf_2a): self
+    {
+        $this->sddnayf_2a = $sddnayf_2a;
+        return $this;
+    }
+
+    public function getSddnayf2d(): ?int
+    {
+        return $this->sddnayf_2d;
+    }
+
+    public function setSddnayf2d(?int $sddnayf_2d): self
+    {
+        $this->sddnayf_2d = $sddnayf_2d;
+        return $this;
+    }
+
+    public function getSddnayf2fa(): ?\DateTimeInterface
+    {
+        return $this->sddnayf_2fa;
+    }
+
+    public function setSddnayf2fa(?\DateTimeInterface $sddnayf_2fa): self
+    {
+        $this->sddnayf_2fa = $sddnayf_2fa;
+        return $this;
+    }
+
+    public function getSddnayf2fb(): ?\DateTimeInterface
+    {
+        return $this->sddnayf_2fb;
+    }
+
+    public function setSddnayf2fb(?\DateTimeInterface $sddnayf_2fb): self
+    {
+        $this->sddnayf_2fb = $sddnayf_2fb;
+        return $this;
+    }
+
+    public function getSddnayf2g(): ?int
+    {
+        return $this->sddnayf_2g;
+    }
+
+    public function setSddnayf2g(?int $sddnayf_2g): self
+    {
+        $this->sddnayf_2g = $sddnayf_2g;
+        return $this;
+    }
+
+    public function getSddnayf2h(): ?int
+    {
+        return $this->sddnayf_2h;
+    }
+
+    public function setSddnayf2h(?int $sddnayf_2h): self
+    {
+        $this->sddnayf_2h = $sddnayf_2h;
+        return $this;
+    }
+
+    public function getSddnayf2i(): ?bool
+    {
+        return $this->sddnayf_2i;
+    }
+
+    public function setSddnayf2i(?bool $sddnayf_2i): self
+    {
+        $this->sddnayf_2i = $sddnayf_2i;
+        return $this;
+    }
+
+    //add remove
+    // sddnayf_1b
+    public function getSddnayf1b(): Collection
+    {
+        return $this->sddnayf_1b;
+    }
+
+    public function addSddnayf1b(Nomenclador $item): self
+    {
+        if (!$this->sddnayf_1b->contains($item)) {
+            $this->sddnayf_1b->add($item);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf1b(Nomenclador $item): self
+    {
+        $this->sddnayf_1b->removeElement($item);
+        return $this;
+    }
+
+    // sddnayf_1c
+    public function getSddnayf1c(): Collection
+    {
+        return $this->sddnayf_1c;
+    }
+
+    public function addSddnayf1c(Nomenclador $item): self
+    {
+        if (!$this->sddnayf_1c->contains($item)) {
+            $this->sddnayf_1c->add($item);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf1c(Nomenclador $item): self
+    {
+        $this->sddnayf_1c->removeElement($item);
+        return $this;
+    }
+
+    // sddnayf_1e
+    public function getSddnayf1e(): Collection
+    {
+        return $this->sddnayf_1e;
+    }
+
+    public function addSddnayf1e(Nomenclador $item): self
+    {
+        if (!$this->sddnayf_1e->contains($item)) {
+            $this->sddnayf_1e->add($item);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf1e(Nomenclador $item): self
+    {
+        $this->sddnayf_1e->removeElement($item);
+        return $this;
+    }
+
+    // sddnayf_2b
+    public function getSddnayf2b(): Collection
+    {
+        return $this->sddnayf_2b;
+    }
+
+    public function addSddnayf2b(Nomenclador $item): self
+    {
+        if (!$this->sddnayf_2b->contains($item)) {
+            $this->sddnayf_2b->add($item);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf2b(Nomenclador $item): self
+    {
+        $this->sddnayf_2b->removeElement($item);
+        return $this;
+    }
+
+    // sddnayf_2c
+    public function getSddnayf2c(): Collection
+    {
+        return $this->sddnayf_2c;
+    }
+
+    public function addSddnayf2c(Nomenclador $item): self
+    {
+        if (!$this->sddnayf_2c->contains($item)) {
+            $this->sddnayf_2c->add($item);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf2c(Nomenclador $item): self
+    {
+        $this->sddnayf_2c->removeElement($item);
+        return $this;
+    }
+
+    // sddnayf_2e
+    public function getSddnayf2e(): Collection
+    {
+        return $this->sddnayf_2e;
+    }
+
+    public function addSddnayf2e(Nomenclador $item): self
+    {
+        if (!$this->sddnayf_2e->contains($item)) {
+            $this->sddnayf_2e->add($item);
+        }
+        return $this;
+    }
+
+    public function removeSddnayf2e(Nomenclador $item): self
+    {
+        $this->sddnayf_2e->removeElement($item);
+        return $this;
+    }
+
+    //hijosVictima
+    public function getHijosVictima(): Collection
+    {
+        return $this->hijosVictima;
+    }
+
+    public function addHijoVictima(SddnayfHijosVictima $hijo): self
+    {
+        if (!$this->hijosVictima->contains($hijo)) {
+            $this->hijosVictima[] = $hijo;
+            $hijo->setSddnayfNew($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHijoVictima(SddnayfHijosVictima $hijo): self
+    {
+        if ($this->hijosVictima->removeElement($hijo)) {
+            // set the owning side to null (unless already changed)
+            if ($hijo->getSddnayfNew() === $this) {
+                $hijo->setSddnayfNew(null);
+            }
+        }
+
+        return $this;
+    }
+}
